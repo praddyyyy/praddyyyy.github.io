c5d7791ceaea5ace75e154990eb6254f06fa7f98
Pradeeshwar <vadai.18@protonmail.com>
2022-01-12 11:47:14 +0530

    Enable linked lines

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ particlesJS('particles-js',
         }
       },
       "size": {
-        "value": 5,
+        "value": 2,
         "random": true,
         "anim": {
           "enable": false,
@@ -61,7 +61,7 @@ particlesJS('particles-js',
         }
       },
       "line_linked": {
-        "enable": false,
+        "enable": true,
         "distance": 150,
         "color": "#ffffff",
         "opacity": 0.4,
