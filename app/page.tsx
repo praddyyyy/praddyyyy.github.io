d184b90436d88286e52e07e865b620cb54bba14f
Vadai <mtech339@gmail.com>
2023-09-29 18:01:27 +0530

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import HeroSection from "@/components/HeroSection/HeroSection";
 import Container from "@/components/ui/container";
 
 export default function Home() {
   return (
     <Container>
+      <HeroSection />
     </Container>
   );
 }
