f691441e6f9078b5cb2cdc30938a9739b86814bc
Pradeeshwar <pradeeshxdev@gmail.com>
2023-09-30 15:16:48 +0530

    add sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
+import AboutSection from "@/components/AboutSection/AboutSection";
+import AchievementSection from "@/components/AchievementSection/AchievementSection";
+import ContactSection from "@/components/ContactSection/ContactSection";
+import EducationSection from "@/components/EducationSection/EducationSection";
 import HeroSection from "@/components/HeroSection/HeroSection";
+import ProjectsSection from "@/components/ProjectsSection/ProjectsSection";
+import SkillsSection from "@/components/SkillsSection/SkillsSection";
 import Container from "@/components/ui/container";
 
 export default function Home() {
   return (
     <Container>
-      <HeroSection />
+      <section id="hero">
+        <HeroSection />
+      </section>
+      <section id="about">
+        <AboutSection />
+      </section>
+      <section id="education">
+        <EducationSection />
+      </section>
+      <section id="skills">
+        <SkillsSection />
+      </section>
+      <section id="projects">
+        <ProjectsSection />
+      </section>
+      <section id="achievements">
+        <AchievementSection />
+      </section>
+      <section id="contact">
+        <ContactSection />
+      </section>
     </Container>
   );
 }
