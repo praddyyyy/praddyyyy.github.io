4487f3ed7df3066c2d990accf9e57ff1cca94c4d
Vadai <mtech339@gmail.com>
2023-10-07 10:32:42 +0530

    add react scroll

--- components/AboutSection/AboutSection.tsx
+++ components/AboutSection/AboutSection.tsx
@@ -1,23 +1,44 @@
+"use client";
+
 import Image from "next/image";
 import about from "../../public/images/about.svg";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useEffect } from "react";
 
 const AboutSection = () => {
+  const { ref, inView } = useInView();
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("About");
+      console.log("in view");
+    }
+  }, [inView, setActiveSection]);
+
   return (
-    <div className="h-screen flex flex-col items-center justify-evenly">
+    <section
+      id="about"
+      ref={ref}
+      className="h-screen mt-8 flex flex-col items-center justify-evenly"
+    >
       <p className="text-2xl tracking-widest font-bold">ABOUT ME</p>
       <div className="flex flex-col sm:flex-row justify-evenly items-center w-full">
-        <Image src={about} alt="about" />
-        <div className="max-w-[250px] sm:max-w-[500px]">
-          <p className="text-lg font-light">
+        <div className="w-[200px] h-[200px] sm:h-[300px] sm:w-[300px] lg:h-[400px] lg:w-[400px]">
+          <Image priority src={about} alt="about" />
+        </div>
+        <div className="py-10 sm:py-0 max-w-[300px] md:max-w-[350px] lg:max-w-[500px]">
+          <p className="text-md text-justify sm:text-lg font-light">
             I am an ambitious individual, with an analytical mind and a strong
             academic background. I am currently pursuing 2 Bachelors Degree,
             namely,{" "}
-            <span className="font-bold">
+            <span className="font-semibold sm:font-bold">
               B.E in Electronics and Communications at SSN College of
               Engineering
             </span>{" "}
             and{" "}
-            <span className="font-bold">
+            <span className="font-semibold sm:font-bold">
               B.Sc. in Data Science and Applications at IITM
             </span>
             . I am a fast learner and have a unique perspective in general. That
@@ -26,7 +47,7 @@ const AboutSection = () => {
           </p>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
