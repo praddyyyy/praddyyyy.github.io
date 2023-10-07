ae6708ea55a2f4a85901b599ec998f0381c14e72
Vadai <mtech339@gmail.com>
2023-10-07 10:32:18 +0530

    Add react scroll context provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Header from "@/components/Header";
+import HeaderNew from "@/components/ui/HeaderNew";
+import ActiveSectionContextProvider from "@/context/active-section-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +19,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="!scroll-smooth">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light">
-          <Header />
-          {children}
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <ActiveSectionContextProvider>
+            <HeaderNew />
+            {children}
+          </ActiveSectionContextProvider>
         </ThemeProvider>
       </body>
     </html>
