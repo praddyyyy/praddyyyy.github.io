2203ae7f0de9ab317db4bf9b715d8226f9cc71c5
Vadai <mtech339@gmail.com>
2023-11-26 10:39:50 +0530

    Update AchievementSection component

--- components/AchievementSection/AchievementSection.tsx
+++ components/AchievementSection/AchievementSection.tsx
@@ -1,8 +1,11 @@
 const AchievementSection = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center -mt-[73px] pt-[73px]">
-      <p>Achievements</p>
-    </div>
+    <section
+      id="achievements"
+      className="h-screen flex flex-col items-center justify-center scroll-mt-28"
+    >
+      <p>Will be updated shortly!</p>
+    </section>
   );
 };
 
