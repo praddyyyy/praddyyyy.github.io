4a4a5079ca9c0add1b705a02efc9c6ec81b7ec71
Vadai <mtech339@gmail.com>
2023-11-26 10:40:08 +0530

    Add social media icons and section in view hook

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -2,8 +2,15 @@
 
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import { Button } from "../ui/button";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { ArrowDownToLine } from "lucide-react";
+import Link from "next/link";
+import { useSectionInView } from "@/lib/hooks";
 
 const HeroSection = () => {
+  const { ref } = useSectionInView("Home", 0.5);
+
   const [text, count] = useTypewriter({
     words: [
       "<Developer />",
@@ -17,7 +24,11 @@ const HeroSection = () => {
   });
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <section
+      ref={ref}
+      className="h-screen flex flex-col items-center justify-center"
+      id="home"
+    >
       <div className="text-3xl sm:text-6xl text-center font-bold">
         <p className="text-2xl sm:text-5xl">Hi👋,</p>
         <p>
@@ -28,10 +39,35 @@ const HeroSection = () => {
         <span>{text}</span>
         <Cursor cursorColor="#10b981" />
       </h1>
-      <div className="py-8">
-        <Button className="bg-teal-500 hover:bg-teal-700">My Resume</Button>
+      <div className="py-8 flex flex-row w-[30rem] justify-evenly items-stretch">
+        <Button className="bg-teal-500 hover:bg-teal-700">
+          My Resume <ArrowDownToLine className="h-5 w-5 ml-3" />
+        </Button>
+        <div>
+          <Button
+            size="icon"
+            variant="outline"
+            className="mr-3 rounded-full bg-white/10"
+          >
+            <Link target="_blank" href="https://www.github.com/praddyyyy/">
+              <FaGithub className="h-6 w-6" />
+            </Link>
+          </Button>
+          <Button
+            size="icon"
+            variant="outline"
+            className="rounded-full bg-white/10"
+          >
+            <Link
+              target="_blank"
+              href="https://www.linkedin.com/in/pradeeshwar/"
+            >
+              <FaLinkedin className="h-6 w-6" />
+            </Link>
+          </Button>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
