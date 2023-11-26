a91edf7e7edb9436a7e52e101c06731202e0ee13
Vadai <mtech339@gmail.com>
2023-11-26 10:40:24 +0530

    Remove commented out code for Achievements section

--- components/ui/HeaderNew.tsx
+++ components/ui/HeaderNew.tsx
@@ -26,10 +26,10 @@ export const routes = [
     hash: "#skills",
     label: "Skills",
   },
-  {
-    hash: "#achievements",
-    label: "Achievements",
-  },
+  // {
+  //   hash: "#achievements",
+  //   label: "Achievements",
+  // },
   {
     hash: "#contact",
     label: "Contact",
@@ -37,13 +37,14 @@ export const routes = [
 ];
 
 const HeaderNew = () => {
-  const { activeSection, setActiveSection } = useActiveSectionContext();
+  const { activeSection, setActiveSection, setTimeOfLastClick } =
+    useActiveSectionContext();
   return (
     <header className="z-[999] relative">
       <motion.div
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-sm border border-teal-500 border-opacity-50 bg-teal-500 bg-opacity-50 shadow-lg shadow-black/[0/3] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[40rem] sm:rounded-full"
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-sm border border-teal-500 border-opacity-50 bg-teal-500 bg-opacity-50 shadow-lg shadow-black/[0/3] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[30rem] sm:rounded-full"
       ></motion.div>
       <nav className="flex fixed top-[0.3rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-200 sm:w-[initial] sm:flex-nowrap sm:gap-5">
@@ -56,16 +57,19 @@ const HeaderNew = () => {
             >
               <Link
                 href={link.hash}
-                onClick={() => setActiveSection(link.hash)}
+                onClick={() => {
+                  setActiveSection(link.label);
+                  setTimeOfLastClick(Date.now());
+                }}
                 className={clsx(
                   "flex w-full items-center justify-center px-3 py-3 hover:text-black transition",
                   {
-                    "text-black": activeSection === link.hash,
+                    "text-black": activeSection === link.label,
                   }
                 )}
               >
                 {link.label}
-                {link.hash === activeSection && (
+                {link.label === activeSection && (
                   <motion.span
                     layoutId="activeLink"
                     className="bg-teal-300 rounded-full absolute inset-0 -z-10"
