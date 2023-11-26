b3aa17601bb09beb448dc69b9fa68cbbce346d04
Vadai <mtech339@gmail.com>
2023-11-26 10:40:02 +0530

    Update EducationSection component styling

--- components/EducationSection/EducationSection.tsx
+++ components/EducationSection/EducationSection.tsx
@@ -1,7 +1,7 @@
 const EducationSection = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center -mt-[73px] pt-[73px]">
-      <p>Education</p>
+    <div className="h-screen flex flex-col items-center justify-center">
+      <p className="text-2xl tracking-widest font-bold">EDUCATION</p>
     </div>
   );
 };
