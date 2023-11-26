0db8bd269872eecd8b3997d23709b1054446f18d
Vadai <mtech339@gmail.com>
2023-11-26 10:39:57 +0530

    Add contact form to ContactSection component

--- components/ContactSection/ContactSection.tsx
+++ components/ContactSection/ContactSection.tsx
@@ -1,8 +1,130 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Textarea } from "../ui/textarea";
+import { useSectionInView } from "@/lib/hooks";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string().email(),
+  subject: z.string().min(5, {
+    message: "Subject must be at least 5 characters.",
+  }),
+});
+
 const ContactSection = () => {
+  const { ref } = useSectionInView("Contact");
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
-    <div className="h-screen flex flex-col items-center justify-center -mt-[73px] pt-[73px]">
-      <p>Contact Me</p>
-    </div>
+    <section
+      ref={ref}
+      id="contact"
+      className="h-screen flex flex-col items-center justify-center scroll-mt-12"
+    >
+      <p className="text-2xl tracking-widest font-bold py-14">CONTACT ME</p>
+      <div>
+        <p className="mb-10">
+          Please contact me directly at{" "}
+          <a className="underline" href="mailto:pradeeshxdev@gmail.com">
+            pradeeshxdev@gmail.com
+          </a>{" "}
+          or through this form.
+        </p>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <div className="flex flex-row justify-between">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Your name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Your email" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Your email will not be shared with anyone.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="subject"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Subject" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="subject"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      rows={5}
+                      placeholder="Write your message here..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+    </section>
   );
 };
 
